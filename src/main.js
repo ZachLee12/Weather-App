fd40b9b2f2ad02eeaa9b653f31c106ccbec7a4f6
ZachLee12 <leezhengyang22@gmail.com>
2023-02-03 15:45:30 +0100

    Add favicon

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './assets/styles/style.css'
 import Search from './assets/images/search.png'
 import Loading from './assets/images/loading.gif'
 import DOMCache from './DOMCache'
+import Favicon from './assets/images/cloudy.png'
 import ViewHandler from './ViewHandler'
 
 (new ViewHandler());
